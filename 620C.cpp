a21bb6d162dfc8fcdde20dd1fe87b4fc64d881ca
neilkakkar <neilkakkar@gmail.com>
2016-01-22 20:43:34 +0530

    modify code

--- 620C.cpp
+++ 620C.cpp
@@ -4,6 +4,7 @@
 using namespace std;
 
 int A[300006];
+pair<int,int> Ans[1234567];
 int main()
 {
     ios_base::sync_with_stdio(0);
@@ -13,17 +14,37 @@ int main()
     {
         cin>>A[i];
     }
-    string s="-1";
-    int pointer=1;
-    int start=1;
     map<int,int> x;
-    while(pointer!=N)
+    int start=1;
+    int cnt=0;
+    for(int i=1;i<=N;i++)
     {
-        if(x.find(A[pointer])
-           {
-
-           }
+        if(x.find(A[i])!=x.end())
+        {
+            Ans[cnt]=make_pair(start,i);
+            start=i+1;
+            cnt++;
+            x.clear();
+        }
+        else
+        {
+            x[A[i]]++;
+        }
 
     }
+    if(cnt==0)
+    {
+        cout<<"-1"<<endl;
+    }
+    else{
+        Ans[cnt-1].second = N;
+
+        cout<<cnt<<endl;
+        for(int i=0;i<cnt;i++)
+        {
+            cout<<Ans[i].first<<" "<<Ans[i].second<<endl;
+        }
+    }
+
 }
 
