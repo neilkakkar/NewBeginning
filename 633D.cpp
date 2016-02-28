092927976f2312da9b66640318f26c84729de463
neilkakkar <neilkakkar@gmail.com>
2016-02-28 22:49:44 +0530

    code

--- 633D.cpp
+++ 633D.cpp
@@ -3,6 +3,8 @@
 using namespace std;
 #define sz (int)1e3+5
 int A[sz];
+
+map<int, int> X;
 int main()
 {
     int n;
@@ -10,16 +12,49 @@ int main()
     for(int i=0;i<n;i++)
     {
         cin>>A[i];
+        X[A[i]]++;
     }
-    sort(A,A+n);
+    int ans=0;
     for(int i=0;i<n;i++)
     {
-        for(int j=i+1;j<n;j++)
+        for(int j=0;j<n;j++)
         {
-
+            if(i==j)
+            {
+                continue;
+            }
+            long long a = A[i];
+            long long b = A[j];
+            vector<int> R;
+            if(a==0 && b==0)
+            {
+                ans = max(ans,X[a]);
+                continue;
+            }
+            X[a]--;
+            X[b]--;
+            R.push_back(a);
+            R.push_back(b);
+            long long c = a+b;
+            int temp=2;
+            while(X[c]>0)
+            {
+                X[c]--;
+                R.push_back(c);
+                temp++;
+                a=b;
+                b=c;
+                c=a+b;
+            }
+            ans = max(ans,temp);
+            for(int z=0;z<R.size();z++)
+            {
+                X[R[z]]++;
+            }
         }
 
     }
+    cout<<ans;
 
 
 }
