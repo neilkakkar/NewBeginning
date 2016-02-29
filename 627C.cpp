e5b1d04479b831f10bd99b85d1b95c05792b2c3a
neilkakkar <neilkakkar@gmail.com>
2016-02-29 12:16:37 +0530

    update

--- 627C.cpp
+++ 627C.cpp
@@ -5,21 +5,40 @@ using namespace std;
 int main()
 {
     //freopen("input.txt","r",stdin);
-    long long s,x;
+    long long s,a,b,x,xx;
     cin>>s>>x;
-    /*(if(((s^x)&1)==1)
-    {
-        cout<<"0\n";
-        //return 0;
-    }*/
-    long long ans=0;
+    xx=x;
+    long long ans=1;
     long long i=0;
-    for(i=1;i<s;i++)
+    a = s-x;
+    a/=2;
+    b = (long long)log2(s);
+    if(a!=0)
     {
-        if(x == (i^(s-i)) )
+        b = max(b,(long long)log2(a));
+    }
+    while(i<=b)
+    {
+        if(a&1 && x&1 )
         {
-            ans++;
+            cout<<"0";
+            return 0;
         }
+        if(x&1)
+        {
+            ans*=2;
+        }
+            a/=2;x/=2;i++;/*
+        else if(x&1==0)
+        {
+            a/=2;x/=2;i++;
+        }*/
+
+    }
+    if(xx==s)
+    {
+        //cout<<"wohfo";
+        ans-=2;
     }
     cout<<ans;
 
