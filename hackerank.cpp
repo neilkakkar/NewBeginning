2f6b31b1945b8b4913f3cfa8a551ada778c388f2
neilkakkar <neilkakkar@gmail.com>
2016-05-01 22:34:32 +0530

    hackerank

--- hackerank.cpp
+++ hackerank.cpp
@@ -1,40 +1,27 @@
-#include <cmath>
-#include <cstdio>
-#include <vector>
-#include <iostream>
-#include <algorithm>
+#include <bits/stdc++.h>
 using namespace std;
 
-#define sz (int)1e5+5
+#define sz (int)1e5
 
 int A[sz];
-int sum[sz];
+int main(){
+    int Q;
+    cin >> Q;
+    for(int a0 = 0; a0 < Q; a0++){
+        long L;
+        long R;
+        cin >> L >> R;
 
-int main() {
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
-double A[10]={95,85,80,70,60};
-double B[10] = {85,95,70,65,70};
-//check
-double ans=0;
-double sumA=0;
-double sumB=0;
-int N=5;
-    for(int i=0;i<N;i++){
-        ans+=A[i]*B[i];
-        sumA+=A[i];
-        sumB+=B[i];
+        A[0]=0;
+        for(int i=1;i<1000;i++){
+            A[i] = A[i-1]^i;
+            //cout<<ans<<endl;
+        }
+        int ans=0;
+        for(int i=L;i<=R;i++){
+            ans^=A[i];
+        }
+        cout<<ans<<endl;
     }
-    cout<<ans<<" "<<sumA<<" "<<sumB<<endl;
-    double sumA2=0;
-    double sumB2=0;
-    for(int i=0;i<N;i++){
-        sumA2+=A[i]*A[i];
-        sumB2+=B[i]*B[i];
-    }
-    cout<<sumA2<<" "<<sumB2<<endl;
-
-    double fina = (N*ans-sumA*sumB)/((N*sumA2-sumA*sumA));
-    cout<<fina<<endl;
-    double intercept = (sumB - fina*sumA)/N;
-    cout<<intercept+80*fina<<endl;
+    return 0;
 }
